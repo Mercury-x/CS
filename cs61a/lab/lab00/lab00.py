016c848840c25e31a1484890f6b729993c2710da
Mercury-x <1612395322@qq.com>
2022-09-04 11:44:26 +0800

    setup

---
+++ cs61a/lab/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    twtw = 2 * 3 * 4 * 5 * 5 * 5 - 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 + 44
+    return twtw
