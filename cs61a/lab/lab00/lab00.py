0cded9f1c60cb3e26e7bcc89b0d7c7d807b3853e
Mercury-x <1612395322@qq.com>
2022-09-05 13:04:47 +0800

    cs61a/22fall

--- cs61a/lab/lab00/lab00.py
+++ cs61a/lab/lab00/lab00.py
@@ -1,9 +1,8 @@
-def twenty_twenty():
-    """Come up with the most creative expression that evaluates to 2020,
+def twenty_twenty_two():
+    """Come up with the most creative expression that evaluates to 2022,
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_twenty()
-    2020
+    >>> twenty_twenty_two()
+    2022
     """
-    twtw = 2 * 3 * 4 * 5 * 5 * 5 - 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 + 44
-    return twtw
+    return 2 + 3 + 4 * 5 * 6 + 7 * 9 * 10 * 11 - 12 * 13 * 14 - 2849
--- cs61a/lab/lab00/tests/python-basics.py
+++ cs61a/lab/lab00/tests/python-basics.py
@@ -7,16 +7,21 @@ test = {
         {
           'code': r"""
           >>> 10 + 2
-          12
+          4d9a9f010a771e1434934b7e874ade0f
+          # locked
           >>> 7 / 2
-          3.5
+          c4a5533ecee83953f8fa3965bd6d64cc
+          # locked
           >>> 7 // 2
-          3
-          >>> 7 % 2			# 7 modulo 2, the remainder when dividing 7 by 2.
-          1
+          be9b9b860e53fef39d5863d61f03d3c4
+          # locked
+          >>> 7 % 2  # 7 modulo 2, the remainder when dividing 7 by 2.
+          6f717e91fe5a90641e44dc5a5368b663
+          # locked
           """,
           'hidden': False,
-          'locked': False
+          'locked': True,
+          'multiline': False
         }
       ],
       'scored': False,
@@ -28,19 +33,24 @@ test = {
           'code': r"""
           >>> x = 20
           >>> x + 2
-          22
+          3d3ab69a0677d75a0ef4a99e0d2d1451
+          # locked
           >>> x
-          20
+          e1ac00f801290865dd772310ea7c60e4
+          # locked
           >>> y = 5
           >>> y = y + 3
           >>> y * 2
-          16
+          309984ef0dc06025a91b127042939a0e
+          # locked
           >>> y = y // 4
-          >>> y + x
-          22
+          >>> y + x # think carefully about what x is equal to!
+          3d3ab69a0677d75a0ef4a99e0d2d1451
+          # locked
           """,
           'hidden': False,
-          'locked': False
+          'locked': True,
+          'multiline': False
         }
       ],
       'scored': False,
