78e3c4d1ca89d557ecd2b65335bb7ef6fed60c35
larry-xue <azoux0311@gmail.com>
2022-12-27 23:00:10 +0800

    feat: hw07 complete

--- cs61a/HW/hw07/hw07.scm
+++ cs61a/HW/hw07/hw07.scm
@@ -1,11 +1,24 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
 
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst)
+  (cond 
+    ((null? asc-lst)
+     #t) ; empty list
+    ((null? (cdr asc-lst))
+     #t) ; length = 1
+    ((<= (car asc-lst) (cadr asc-lst))
+     (ascending? (cdr asc-lst)))
+    ((> (car asc-lst) (cadr asc-lst))
+     #f)))
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (cond 
+    ((= exp 1)         base)
+    ((odd? exp)  (* base (pow base (- exp 1))))
+    ((even? exp) (square (pow base (/ exp 2))))))
